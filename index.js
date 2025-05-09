515d4cf4bf4686f1ff97f804c2107539ec693ed7
ameer akech <ameerakech@gmail.com>
2025-05-09 08:51:15 +0300

    second commit

--- index.js
+++ index.js
@@ -7,20 +7,17 @@ function FeatureToggle(featureName,isEnabled,userGroupAccess){
 }
 
 FeatureToggle.prototype.canAccess = function(userRole){
-if (`{this.isEnabled}`)return false;{
 return this.userGroupAccess.includes(userRole)
-}
+
 }
 
 FeatureToggle.prototype.toggleFeature = function(flag){
 this.isEnabled = flag
 }
 
-const feature = FeatureToggle("Instagram",false["betaTester","admins"])
+const feature = new FeatureToggle("Instagram",false,["betaTester","admins"])
 console.log(feature.canAccess("betaTesters"));
 feature.toggleFeature(true)
-console.log(feature.canAccess("betaTesters"))
-console.log(feature.canAccess("regular"))
 
 
 //QUESTION TWO
@@ -29,7 +26,55 @@ function Timelog(freelancerName,projectDetails,logs){
     this.freelancerName = freelancerName
     this.projectDetails = projectDetails
     this.logs = logs
-}
-Timelog.prototype.totalEarnings= function(){
 
 }
+Timelog.prototype.totalEarnings= function(){
+return this.logs.reduce ((total,log)=>total+loghours)
+
+}
+
+//QUESTION FIVE
+class Course {
+    constructor(title, instructor, students) {
+      this.title = title;
+      this.instructor = instructor; 
+      this.students = students; 
+    }
+
+    getCompletedStudents() {
+      return this.students
+        .filter(student => student.completionStatus)
+        .map(student => student.name);
+    }
+    countStudentsByExpertise(expertise) {
+      return this.students.filter(
+        student => this.instructor.expertise === expertise
+      ).length;
+    }
+    instructorStudentMessage() {
+      const studentCount = this.students.length;
+      switch (true) {
+        case studentCount > 5:
+          return `${this.instructor.name} has a large class of ${studentCount} students!`;
+        case studentCount > 0:
+          return `${this.instructor.name} is teaching ${studentCount} students.`;
+        default:
+          return `${this.instructor.name} has no students yet.`;
+      }
+    }
+  }
+  
+  const course = new Course(
+    "JavaScript Basics",
+    { name: "Ameer", expertise: "Programming" },
+    [
+      { name: "John", completionStatus: true },
+      { name: "Mary", completionStatus: false },
+      { name: "Susan", completionStatus: true }
+    ]
+  );
+  console.log(course.getCompletedStudents());
+  console.log(course.countStudentsByExpertise("Programming")); 
+  console.log(course.instructorStudentMessage()); 
+
+
